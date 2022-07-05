5b3e44f702ad07bf1f33a4ddbe7a731574d1f432
TirthakBM <tirthakbm123@gmail.com>
2022-07-06 00:51:31 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { RectButton, CircleButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from './DetailsDesc';
+import { SubInfo } from "./SubInfo";
+
+export {
+    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton, DetailsBid, DetailsDesc, SubInfo
+}
